1caca35addc57b7e42489e95724b600ec39aed06
Joseph Grabski <josephgrabski31@gmail.com>
2025-03-13 23:21:09 +0000

    Basic physics integration with avian2d

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-use bevy::prelude::*;
+use avian2d::prelude::*;
+use bevy::{math::vec2, prelude::*};
+
+#[derive(Component)]
+struct Ball;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(Gravity(Vec2::NEG_Y * 100.0))
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        .add_systems(Startup, (setup, spawn_ball))
+        .run();
+}
+
+fn spawn_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let shape = meshes.add(Circle::new(50.0));
+    let material = materials.add(ColorMaterial::from(Color::srgb(1.0, 0.0, 0.0)));
+    commands.spawn((
+        Ball,
+        Mesh2d(shape),
+        MeshMaterial2d(material),
+        RigidBody::Dynamic,
+        Collider::circle(50.0),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
 }
